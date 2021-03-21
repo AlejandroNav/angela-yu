1031b5570ac8773631c0f4db47a73e52f1c32731
Alex-Nav <alejugger@gmail.com>
2021-03-21 14:54:25 -0600

    adding links to index

--- js/dom.js
+++ js/dom.js
@@ -1,12 +1,17 @@
 const resultado = document.querySelector('.resul')
-const boton = document.querySelector('.btn')
-const numero = 15
+const boton2 = document.querySelector('.btn2')
+const boton1 = document.querySelector('.btn1')
 
-function fibo(numero) {
+boton1.addEventListener("click", function() {
+    resultado.innerHTML = "Hola que tal"
+})
+
+boton2.addEventListener('click', function() {
+    const numero = 8
     let cuenta = [0, 1]
     for (let index = 0; index < numero; index++) {
         cuenta.push(cuenta[cuenta.length - 1] + cuenta[cuenta.length - 2])
     }
     resultado.innerHTML = cuenta
-}
-boton.addEventListener('click', fibo(numero))
+})
+
