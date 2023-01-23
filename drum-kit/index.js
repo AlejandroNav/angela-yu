225378cfe1a5df37dd20bd3779380c50e056a135
Alex-Nav <alejugger@gmail.com>
2023-01-23 11:43:17 -0600

    sound working

--- drum-kit/index.js
+++ drum-kit/index.js
@@ -3,8 +3,41 @@
 var numberBtn = document.querySelectorAll(".drum").length;
 
 for (let index = 0; index < numberBtn; index++) {
-    document.querySelectorAll(".drum")[index].addEventListener("click", function () {
-        var audio = new Audio("sounds/crash.mp3");
+    document
+    .querySelectorAll(".drum")[index].addEventListener("click", function () {
+        var pressedKey = this.innerHTML;
+        console.log(pressedKey);
+        switch (pressedKey) {
+        case 'w':
+            var audio = new Audio("sounds/crash.mp3");
         audio.play();
+            break;
+        case 'a':
+            var audio = new Audio("sounds/crash.mp3");
+        audio.play();
+            break;
+        case 's':
+            var audio = new Audio("sounds/crash.mp3");
+        audio.play();
+            break;
+        case 'd':
+            var audio = new Audio("sounds/crash.mp3");
+        audio.play();
+            break;
+        case 'j':
+            var audio = new Audio("sounds/crash.mp3");
+        audio.play();
+            break;
+        case 'k':
+            var audio = new Audio("sounds/crash.mp3");
+        audio.play();
+            break;
+        case 'l':
+            var audio = new Audio("sounds/crash.mp3");
+        audio.play();
+            break;
+        default:
+            break;
+        }
     });
 }
