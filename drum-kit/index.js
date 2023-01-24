06889a4c1be8f2e093dc0de3bfb0abc5bf72d0b9
Alex-Nav <alejugger@gmail.com>
2023-01-24 10:04:01 -0600

    refactoring and adding keyboard suport

--- drum-kit/index.js
+++ drum-kit/index.js
@@ -6,38 +6,45 @@ for (let index = 0; index < numberBtn; index++) {
     document
     .querySelectorAll(".drum")[index].addEventListener("click", function () {
         var pressedKey = this.innerHTML;
-        console.log(pressedKey);
-        switch (pressedKey) {
+        makeSound(pressedKey)
+    });
+}
+addEventListener ("keydown",function(evt){
+    makeSound(evt.key)
+})
+
+function makeSound(pressedKey) {
+
+    switch (pressedKey) {
         case 'w':
             var audio = new Audio("sounds/crash.mp3");
         audio.play();
             break;
         case 'a':
-            var audio = new Audio("sounds/crash.mp3");
-        audio.play();
+            var audio2 = new Audio("sounds/kick-bass.mp3");
+        audio2.play();
             break;
         case 's':
-            var audio = new Audio("sounds/crash.mp3");
+            var audio = new Audio("sounds/snare.mp3");
         audio.play();
             break;
         case 'd':
-            var audio = new Audio("sounds/crash.mp3");
+            var audio = new Audio("sounds/tom-1.mp3");
         audio.play();
             break;
         case 'j':
-            var audio = new Audio("sounds/crash.mp3");
+            var audio = new Audio("sounds/tom-2.mp3");
         audio.play();
             break;
         case 'k':
-            var audio = new Audio("sounds/crash.mp3");
+            var audio = new Audio("sounds/tom-3.mp3");
         audio.play();
             break;
         case 'l':
-            var audio = new Audio("sounds/crash.mp3");
+            var audio = new Audio("sounds/tom-4.mp3");
         audio.play();
             break;
         default:
             break;
-        }
-    });
+    }
 }
