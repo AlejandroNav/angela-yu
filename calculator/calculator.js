6824a5f430dbe25fa424222e56dfacbb8f7d6c10
Alex-Nav <alejugger@gmail.com>
2023-02-05 10:15:27 -0600

    finish basic sum

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/",function (req,res) {
-    res.sendFile(__dirname + "/index.html")
+    res.sendFile(__dirname + "/index.html");
 });
-
+app.post("/", function (requ,res) {
+    console.log(requ.body);
+    var num1 = parseInt(requ.body.numero1) 
+    var num2 = parseInt(requ.body.numero2) 
+    var suma = num1 + num2;
+    res.send("Hola estamos escuchando tu post request la suma es  "+suma);
+})
 app.listen(4000,function() {
     console.log("escucha correctamente a node en el 4000  ");
 });
