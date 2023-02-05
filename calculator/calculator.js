f0b4a6c312387f2bbdd64efeeecfea70f82d8fb7
Alex-Nav <alejugger@gmail.com>
2023-02-05 09:38:17 -0600

    restarting calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -3,11 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/",function (req,res) {
-    console.log(__dirname);
     res.sendFile(__dirname + "/index.html")
 });
 
 app.listen(4000,function() {
-    console.log(__dirname);
     console.log("escucha correctamente a node en el 4000  ");
 });
