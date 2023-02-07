c3e55f976b6d488b57f6358512387737ebcf1ef3
Alex-Nav <alejugger@gmail.com>
2023-02-07 12:15:17 -0600

    API data is working

--- weather/app.js
+++ weather/app.js
@@ -2,8 +2,16 @@ const express = require("express");
 
 const app = express();
 
+const https = require('node:https');
 
 app.get("/",function (req,res) {
+    const url1 =  "https://api.open-meteo.com/v1/forecast?latitude=23.00&longitude=-102.00&current_weather=true"
+    https.get(url1,function (response) {
+        response.on("data", function (data) {
+            const waeData = JSON.parse(data)
+            console.log(waeData.current_weather.temperature); 
+        })
+    });
     res.send("server is up and runnign")
 })
 
