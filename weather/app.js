ba0ee285c9fc12769a7bfd85ea2566a18a0eada0
Alex-Nav <alejugger@gmail.com>
2023-02-08 11:46:35 -0600

    finished weather app

--- weather/app.js
+++ weather/app.js
@@ -9,23 +9,20 @@ app.get("/",function (req,res) {
     res.sendFile(__dirname+"/index.html")
 })
 app.post("/", function(req,res) {
-    console.log(req.body);
-    console.log(req.body.longitude2);
+    const lat = req.body.latitude1
+    const long = req.body.longitude2
+    const url1 =  "https://api.open-meteo.com/v1/forecast?latitude="+lat+"&longitude="+long+"&current_weather=true"
+    https.get(url1,function (response) {
+        response.on("data", function (data) {
+            const waeData = JSON.parse(data)
+            res.write("<h1> La temperatura en "+long+","+lat +' es de ' + waeData.current_weather.temperature + " grados Centigrados </h1>")
+            res.write('<h3> El clima es ' + waeData.current_weather.weathercode + " del codigo </h3>")
+            res.send()
+        })
+    }); 
     
 })
 
-/* const lat = "25"
-const long = "-99"
-const url1 =  "https://api.open-meteo.com/v1/forecast?latitude="+lat+"&longitude="+long+"&current_weather=true"
-https.get(url1,function (response) {
-    response.on("data", function (data) {
-        const waeData = JSON.parse(data)
-        console.log(waeData); 
-        res.write('<h1> La temperatura en CDMX es de ' + waeData.current_weather.temperature + " grados Centigrados </h1>")
-        res.write('<h3> El clima es ' + waeData.current_weather.weathercode + " del codigo </h3>")
-        res.send()
-    })
-}); */
 
 app.listen(3001,function () {
     console.log("funciona el server en 3001");
