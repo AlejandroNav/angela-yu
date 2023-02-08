5843b88f6cc01dc3e73e76a5e6065011bd508b32
Alex-Nav <alejugger@gmail.com>
2023-02-08 10:50:31 -0600

    get livedata get, respond with send

--- weather/app.js
+++ weather/app.js
@@ -5,14 +5,16 @@ const app = express();
 const https = require('node:https');
 
 app.get("/",function (req,res) {
-    const url1 =  "https://api.open-meteo.com/v1/forecast?latitude=23.00&longitude=-102.00&current_weather=true"
+    const url1 =  "https://api.open-meteo.com/v1/forecast?latitude=19.00&longitude=-99.00&current_weather=true"
     https.get(url1,function (response) {
         response.on("data", function (data) {
             const waeData = JSON.parse(data)
-            console.log(waeData.current_weather.temperature); 
+            console.log(waeData); 
+            res.write('<h1> La temperatura en CDMX es de ' + waeData.current_weather.temperature + " grados Centigrados </h1>")
+            res.write('<h3> El clima es ' + waeData.current_weather.weathercode + " del codigo </h3>")
+            res.send()
         })
     });
-    res.send("server is up and runnign")
 })
 
 app.listen(3001,function () {
