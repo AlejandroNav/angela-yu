4388be37f8cd032908339f25c78c72c61197ade4
Alex-Nav <alejugger@gmail.com>
2023-02-08 11:38:11 -0600

    upadting html to take LAT and Long

--- weather/app.js
+++ weather/app.js
@@ -1,21 +1,31 @@
 const express = require("express");
+const https = require('node:https');
 
 const app = express();
 
-const https = require('node:https');
+app.use(express.urlencoded({ extended: true})); 
 
 app.get("/",function (req,res) {
-    const url1 =  "https://api.open-meteo.com/v1/forecast?latitude=19.00&longitude=-99.00&current_weather=true"
-    https.get(url1,function (response) {
-        response.on("data", function (data) {
-            const waeData = JSON.parse(data)
-            console.log(waeData); 
-            res.write('<h1> La temperatura en CDMX es de ' + waeData.current_weather.temperature + " grados Centigrados </h1>")
-            res.write('<h3> El clima es ' + waeData.current_weather.weathercode + " del codigo </h3>")
-            res.send()
-        })
-    });
+    res.sendFile(__dirname+"/index.html")
 })
+app.post("/", function(req,res) {
+    console.log(req.body);
+    console.log(req.body.longitude2);
+    
+})
+
+/* const lat = "25"
+const long = "-99"
+const url1 =  "https://api.open-meteo.com/v1/forecast?latitude="+lat+"&longitude="+long+"&current_weather=true"
+https.get(url1,function (response) {
+    response.on("data", function (data) {
+        const waeData = JSON.parse(data)
+        console.log(waeData); 
+        res.write('<h1> La temperatura en CDMX es de ' + waeData.current_weather.temperature + " grados Centigrados </h1>")
+        res.write('<h3> El clima es ' + waeData.current_weather.weathercode + " del codigo </h3>")
+        res.send()
+    })
+}); */
 
 app.listen(3001,function () {
     console.log("funciona el server en 3001");
